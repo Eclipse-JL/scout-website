8b439e43829a2bfb2395d844e2542c50dd95cb61
Eclipse-JL <jlroboteclipse@gmail.com>
2025-05-04 21:07:47 -0400

    création sections

--- scripts.js
+++ scripts.js
@@ -2,10 +2,12 @@ function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-  }
-  
-  function closeNav() {
+    document.getElementById("overlay").style.display = "block"; // Affiche l'overlay
+}
+
+function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
+    document.getElementById("main").style.marginLeft = "0";
     document.body.style.backgroundColor = "white";
-  }
+    document.getElementById("overlay").style.display = "none"; // Masque l'overlay
+}
