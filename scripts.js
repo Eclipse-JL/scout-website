b30a0adc09182455cb64b5fb6ae135826e116e21
Eclipse-JL <jlroboteclipse@gmail.com>
2025-05-14 20:09:02 -0400

    img repared+ navbutton close

--- scripts.js
+++ scripts.js
@@ -1,8 +1,22 @@
+function toggleNav() {
+    const sidenav = document.getElementById("mySidenav");
+    const overlay = document.getElementById("overlay");
+
+    if (sidenav.style.width === "250px") {
+        // Si le sidenav est ouvert, on le ferme
+        closeNav();
+    } else {
+        // Sinon, on l'ouvre
+        openNav();
+    }
+}
+
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
     document.getElementById("overlay").style.display = "block"; // Affiche l'overlay
+    document.body.classList.add("darken");
 }
 
 function closeNav() {
@@ -10,4 +24,5 @@ function closeNav() {
     document.getElementById("main").style.marginLeft = "0";
     document.body.style.backgroundColor = "white";
     document.getElementById("overlay").style.display = "none"; // Masque l'overlay
+    document.body.classList.remove("darken");
 }
